dad8eeb9d7f3050d78872f0948ad3dd9fe9884f2
dni <daniele@nicastro.io>
2023-02-04 22:30:34 +0100

    lets go

--- app/Jobs/LoginEmailAlert.php
+++ app/Jobs/LoginEmailAlert.php
@@ -5,7 +5,6 @@ namespace App\Jobs;
 use App\Models\Member;
 use App\Services\MailgunSendMail;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
